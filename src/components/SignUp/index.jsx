1865cc95392128427d8632b0b98cd6254540a2fe
Maksim Bazhenov <maksim4k1@gmail.com>
2021-08-14 23:05:34 +0600

    added styles for SignUp component, added title, font-family and shadow color

--- src/components/SignUp/index.jsx
+++ src/components/SignUp/index.jsx
@@ -1,10 +1,26 @@
 import React from "react";
+import styled from "styled-components";
+
+const Title = styled.h1`
+    color: var(--main-black);
+    font-size: 32px;
+    font-weight: 700;
+`;
+
+const SignUpSection = styled.section`
+    width: 500px;
+    padding: 30px;
+    margin: 0 auto;
+    background: var(--main-white);
+    border-radius: 3px;
+    box-shadow: 0 2px 3px var(--main-shadow-black);
+`;
 
 function SignUp () {
     return(
-        <div>
-            
-        </div>
+        <SignUpSection>
+            <Title>Sign Up</Title>
+        </SignUpSection>
     );
 }
 
